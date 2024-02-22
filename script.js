3ca99841a0259e9cfcbd181e1d3ac2af84f34513
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-02-22 15:53:49 +0100

    chore: first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const ligne = 5
+const colone = 5
+const start = document.querySelector("#start")
+start.addEventListener("click", function () {
+const table = document.createElement("table")
+for(let i_ligne = 0; i_ligne <= ligne ; i_ligne++)
+})

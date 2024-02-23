872642e0ed032ee18e03e84011a95825bdf0e892
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-02-23 11:38:56 +0100

    chore: dynamic generation of the color

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function mt_rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 const ligne = 5
 const colone = 5
 const start = document.querySelector("#start")
@@ -8,7 +11,7 @@ start.addEventListener("click", function () {
         const line = document.createElement("tr")
         for (let i_colone = 0; i_colone < colone; i_colone++) {
             const column = document.createElement("td")
-            column.setAttribute("id", i_ligne + "," + i_colone)
+            column.setAttribute("id","l"+i_ligne + "c"+ i_colone)
             line.appendChild(column)
 
         }
@@ -16,9 +19,14 @@ start.addEventListener("click", function () {
     }
     body.appendChild(table)
     start.classList.add("display-none")
-setInterval(1,function() {
-    
-})
+    const td = document.querySelectorAll("td")
+    setInterval(function () {
+        const l = mt_rand(0,colone -1)
+        const c = mt_rand(0,ligne -1)
+        const verte = document.querySelector("#l"+l+"c"+ c)
+        verte.classList.toggle("colorer")
+        console.error(document.querySelectorAll(".colorer").length)
+    }, 1000)
 
 
 

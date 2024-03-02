50aec783e08e7256d00cf451cc7e3d7ed4d671e1
matthieuthuetdev <mthuet.pro@gmail.com>
2024-03-02 16:23:57 +0100

    feat: audio

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function mt_rand(min, max) {
-    return Math.floor(Mathandom() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+let sound_right = false;
+
+const sound_on = document.querySelector("#sound_on");
+
+sound_on.addEventListener("click", function () {
+    sound_on.classList.add("display-none");
+    sound_right = true;
+    const sound = new Audio("./audio/sound.mp3");
+    sound.play();
+});
 
 const ligne = 5;
 const colonne = 5;
@@ -19,45 +30,67 @@ start.addEventListener("click", function () {
     }
     document.body.appendChild(table);
     start.classList.add("display-none");
-    let case_verte = 0;
+    let cases_verte = 0;
     let scor = 0;
     setInterval(function () {
         const td = document.querySelectorAll("td");
         const l = mt_rand(0, ligne - 1);
         const c = mt_rand(0, colonne - 1);
-        const verte = document.querySelector("#l" + l + "c" + c);
-        
-        // Probabilité de 1 chance sur 3 pour que la case soit rouge
-        if (Math.random() < 1/3) {
-            verte.classList.add("rouge");
+        const focus = document.querySelector("#l" + l + "c" + c);
+        if (mt_rand(1, 3) !== 3) {
+            cases_verte++;
+            if (focus.classList.contains("cases_rouge")) {
+                focus.classList.remove("cases_rouge");
+            }
+            focus.classList.add("cases_verte");
         } else {
-            verte.classList.add("colorer");
-            case_verte++;
+            if (focus.classList.contains("cases_verte")) {
+                focus.classList.remove("cases_verte");
+            }
+            focus.classList.add("cases_rouge");
+        }
+        if (sound_right) {
+            const apparition_case_colorer = new Audio("./audio/apparition_case_colorer.mp3");
+            apparition_case_colorer.play();
         }
     }, 1000);
 
     document.querySelectorAll("td").forEach(td => {
         td.addEventListener("click", () => {
             let id = td.id;
-            if (document.querySelector("#" + id).classList.contains("colorer")) {
-                document.querySelector("#" + id).classList.remove("colorer")
+            if (document.querySelector("#" + id).classList.contains("cases_verte")) {
+                document.querySelector("#" + id).classList.remove("cases_verte");
                 scor++;
-                case_verte--;
-            } else if (document.querySelector("#" + id).classList.contains("rouge")) {
-                document.querySelector("#" + id).classList.remove("rouge");
-                scor -= 5; // Retirer 5 points
+                cases_verte--;
+                if (sound_right) {
+                    const case_verte = new Audio("./audio/case_verte.mp3");
+                    case_verte.play();
+                }
+            } else if (document.querySelector("#" + id).classList.contains("cases_rouge")) {
+                document.querySelector("#" + id).classList.remove("cases_rouge");
+                scor = scor - 5;
+                if (sound_right) {
+                    const case_rouge = new Audio("./audio/case_rouge.mp3");
+                    case_rouge.play();
+                }
             } else {
                 scor--;
+                if (sound_right) {
+                    const case_vide = new Audio("./audio/case_vide.wav");
+                    case_vide.play();
+                }
             }
+            const h1 = document.querySelector("h1").innerText = "Score : " + scor + "/25";
         });
     });
+
     setInterval(() => {
-        if (case_verte >= 5) {
+        if (cases_verte >= 5) {
             window.location.href = "./lose.html";
         } else if (scor >= 25) {
             window.location.href = "./win.html";
+        } else if (scor <= -5) {
+            window.location.href = "./lose.html";
         }
-        const h1 = document.querySelector("h1").innerText = "Score : " + scor + "/25"
-
     }, 100);
 });
